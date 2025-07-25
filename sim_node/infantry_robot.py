connect write_robot_state service call. X,Y velocity is tied to the orientation of the base so need to fix the base rotation drifting when it collides with objects

sim_node/infantry_robot.py
@@ -16,8 +16,12 @@ class InfantryRobot(BaseAgent):
     urdf_path = str(
         os.path.join(package_dir, "resource/models/infantry/infantry-blue.urdf")
     )
+
+    default_pos = sapien.Pose(p=[0, 0, 0.25], q=[1, 0, 0, 0])
+    default_pos.set_rpy([np.deg2rad(90), 0, np.deg2rad(180)])
+
     keyframes = dict(
-        default=Keyframe(pose=sapien.Pose(p=[1, 1, 0.25], q=[0.707, 0.707, 0, 0]))
+        default=Keyframe(pose=default_pos)
         # default=Keyframe(pose=sapien.Pose(p=[0, 0, 1.0], q=[1, 0, 0, 0]))
     )
 
@@ -76,7 +80,11 @@ class InfantryRobot(BaseAgent):
 
         controller_configs = dict(
             pd_standard=dict(
-                pitch=pitch_pd_joint, yaw=yaw_pd_joint, base=base_pd_joint_vel
+                # order matters. determines the order of the action space
+                # this is set to match the URDF
+                base=base_pd_joint_vel,
+                yaw=yaw_pd_joint,
+                pitch=pitch_pd_joint,
             )
         )
 

sim_node/simulation.py
@@ -1,29 +1,74 @@
 import gymnasium as gym
 import mani_skill.envs
+from mani_skill.envs.sapien_env import BaseEnv
 from sim_node import comp_field
 import numpy as np
+from sim_node import utils
 
 
 class Simulation:
+
     def __init__(self, seed=2930):
         self.env = gym.make(
             "comp_field",  # This should map to your registered environment
             render_mode="human",  # Use "human" to visualize, or "rgb_array" to render frames without GUI
             reward_mode="sparse",
-            obs_mode="rgb+segmentation+position",
+            obs_mode="state_dict+rgb+segmentation+position",
+            # obs_mode="rgb",
         )
 
-        obs, _ = self.env.reset(seed=seed)  # Reset and get the initial observation
-        keyframe = self.env.unwrapped.agent.keyframes["default"]
-        self.env.unwrapped.agent.robot.set_pose(keyframe.pose)
+        self.base_env: BaseEnv = self.env
+        self.past_obs = None
 
-    def step(self):
-        action = self.env.action_space.sample()  # Take random actions
-        action_test = np.array([0, 0, 0, 0, 0.0])
-        obs, reward, terminated, truncated, info = self.env.step(None)
+        obs, _ = self.env.reset(seed=seed)  # Reset and get the initial observation
+        keyframe = self.env.get_wrapper_attr("agent").keyframes["default"]
+        self.env.get_wrapper_attr("agent").robot.set_pose(keyframe.pose)
+
+    def step(self, desired_robot_state: utils.robot_state):
+        # calculate world relative robot velocity
+        if self.past_obs is not None:
+            world_relative_vel = self.local_to_world_vel(
+                desired_robot_state.x_vel, desired_robot_state.y_vel
+            )
+        else:
+            world_relative_vel = np.array([0, 0])
+
+        action = np.array(
+            [
+                # opposite indexes because I have been given robot cadd with y axis up and this is now my life
+                world_relative_vel[1],
+                world_relative_vel[0],
+                0,
+                desired_robot_state.yaw,
+                desired_robot_state.pitch,
+            ]
+        )
+
+        obs, reward, terminated, truncated, info = self.env.step(action)
         done = terminated or truncated
         self.env.render()
+
+        self.past_obs = obs
         return obs
 
     def shutdown(self):
         self.env.close()
+
+    def local_to_world_vel(self, x_vel, y_vel):
+        yaw_rads = self.past_obs["agent"]["qpos"][0][3].item()
+
+        rotation_matrix = np.array(
+            [
+                [np.cos(yaw_rads), -np.sin(yaw_rads)],
+                [np.sin(yaw_rads), np.cos(yaw_rads)],
+            ]
+        )
+
+        head_relative_vel = np.array([x_vel, y_vel])
+        world_relative_vel = rotation_matrix @ head_relative_vel
+
+        print(
+            f"{yaw_rads:.3f},{x_vel:.3f},{y_vel:.3f},{world_relative_vel[0]:.3f},{world_relative_vel[1]:.3f}"
+        )
+
+        return world_relative_vel