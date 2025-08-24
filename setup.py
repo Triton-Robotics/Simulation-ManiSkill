3f5f8bfdf466725e3bf74d81cc67f1e1938955ad
LuminousLlama <keyushattarde@gmail.com>
2025-08-24 02:20:23 -0700

    add launch file

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ def collect_resource_files(base_dir, package_name):
 data_files = [
     ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
     ("share/" + package_name, ["package.xml"]),
+    ("share/" + package_name + "/launch", glob("launch/*")),
 ]
 
 
